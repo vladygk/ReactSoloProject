d127d6412c1e750c627968ba5e619972aa05b07f
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-13 10:19:46 +0200

    bug fix

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,14 +1,10 @@
+
 import React from 'react'
 
 export default function Logo(props) {
-  // let type;
-  // if(props.type === undefined){
-  //    type = 'default'; 
-  // }else{
-  //   type = props.type ;
-  // }
- const type = props.type !== undefined ? props.type : 'default';
-  
+
+  const {type='default'} = props;
+  console.log(type)
   return (
 
     <img src={type==='muted'? "./images/logo-muted.svg":"./images/logo.svg"} alt="" />
