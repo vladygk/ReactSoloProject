10a4bf69ce27d71b308ce265075bdc518e2017c2
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-13 19:51:58 +0200

    Header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Example from "../src/components/example/Example";
+import Header from "../src/components/header/Header";
 import Logo from "../src/components/logo/Logo"
 export default function Index() {
-  return <Logo  />;
+  return <Header  />;
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Logo from "../logo/Logo";
+import { Grid, Container, Button, Input,startAdornment,InputAdornment,InputProps } from "@mui/material";
+
+ import styles from "./Header.module.scss";
+
+
+export default function Header() {
+  return (
+    <div className={styles.wrapper}>
+      <Container className={styles.container} maxWidth="false">
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid className={styles.logoGrid} item xs={1}>
+        <Logo />
+      </Grid>
+      <Grid className={styles.inputGrid} item xs={6}>
+        
+          <input
+            className={styles.input}
+            placeholder="Find items, users and activities"
+            
+           
+          />
+       
+      </Grid>
+      <Grid className={styles.buttonGrid} item xs={5}>
+        <Button sx={{ color: 'white'}} className={styles.textBtn} variant="text">Home</Button>
+        <Button sx={{ color: 'white'}} variant="text">Activity</Button>
+        <Button className={styles.containedBtn} variant="contained">Explore</Button>
+      </Grid>
+    </Grid>
+    </Container>
+    </div>
+  );
+}
