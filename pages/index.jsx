3a23af9a38e1cf239c2acf09b1a92ba7160fd81e
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-13 21:53:09 +0200

    Add: User component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import Example from "../src/components/example/Example";
 import Header from "../src/components/header/Header";
 import Logo from "../src/components/logo/Logo"
-
+import User from "../src/components/user/User";
 import Avatar from "../src/components/avatar/Avatar";
+
 export default function Index() {
-  return <Avatar size={900}  />;
+  //return <User name="Vaskoi" verified={true} info="134 items"/>;
+  return <User name="Vasko" info="144 items" verified={true}/>
 }
---
+++ src/components/user/User.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Avatar from "../avatar/Avatar";
+import styles from "./User.module.scss";
+
+export default function User({
+  name = "",
+  info = "",
+  avatar = "",
+  size = 55,
+  verified = false,
+}) {
+  return (
+    <div
+      className={styles.user}
+      style={{  width: size * 2.2 }}
+    >
+      <Avatar size={size} verified={verified} />
+      <div className={styles.nameInfo}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.info}>{info}</div>
+      </div>
+    </div>
+  );
+}
