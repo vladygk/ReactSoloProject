81df0f04254b7f3d8692685ad54de97d427ec39f
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-27 23:48:46 +0200

    Add ProductInfoCreator

--- src/components/product/ProductInfoCreator.jsx
+++ src/components/product/ProductInfoCreator.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import User from "../user/User"
+import styles from "./ProductInfoCreator.module.scss"
+
 export default function ProductInfoCreator({
     name,
    avatar,
    verified=false,
    }) {
   return (
-    <div className='product-info-creator'>
-        <label className='title'>Creator</label>
-        <div className='user-container'>
+    <div className={styles['product-info-creator']}>
+        <label className={styles['title']}>Creator</label>
+        <div className={styles['user-container']}>
             <User name={name} avatar={avatar} verified={verified}/>
         </div>
         
