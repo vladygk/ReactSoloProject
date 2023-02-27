bda99226154461b45ee4968bce7cdb52f478dd2d
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-27 23:31:47 +0200

    Add ProductInfoLikes

---
+++ src/components/product/ProductInfoLikes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import millify from "millify";
+
+import styles from "./ProductInfoLikes.module.scss";
+import { Chip } from "@mui/material";
+export default function ProductInfoLikes({ amount = 0 }) {
+  return (
+    <div className={styles["product-info-likes"]}>
+      <div className={styles.likes}><Chip label={millify(amount)}/></div>
+    </div>
+  );
+}
