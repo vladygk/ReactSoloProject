3c32789ccf85444bd10b695f7fc0e9ead35b3cbe
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-27 22:25:11 +0200

    Dir fix

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProductImage({url}) {
   return (
     <div className='product-image'>
-        <img className='image' src="./public/images/nft.jpg" alt="" />
+        <img className='image' src="../../../public/images/nft.jpg" alt="" />
     </div>
   )
 }
