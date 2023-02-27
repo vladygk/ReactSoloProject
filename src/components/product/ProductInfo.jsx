fa8aff05ff0e142b4c6bc99c7512da527d2645dc
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-28 00:06:37 +0200

    fix

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import styles from "./ProductInfo.module.scss"
-export default function ProductInfo() {
+export default function ProductInfo({
+    title,
+    creator: {
+           //"name": String,
+           //"avatar": String,
+           //"verified": Boolean
+    },
+    price,
+    currency,
+    likes,
+    onTimeEnd,
+    timeEnd,
+    isLive
+    }) {
   return (
     <div>ProductInfo</div>
   )
