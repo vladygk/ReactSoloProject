d382187fe24ddcf8a04b3fe7f0144d8342af7511
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-28 01:20:27 +0200

    timer fix 6

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -4,14 +4,19 @@ import Countdown from "react-countdown";
 
 export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
   const isActive = timeEnd ? "active" : "notActive";
-  return (
-    <div className={styles["product-info-timer"]}>
-      <label className={styles["title"]}>Ends in</label>
-      {timeEnd && 
-        <div className={`${styles[isActive]} ${styles["timer"]} `}>
-          <Countdown onComplete={onTimeEnd} date={timeEnd} />
-        </div>
-      }
-    </div>
-  );
+
+  if (isActive) {
+    return (
+      <div className={styles["product-info-timer"]}>
+        <label className={styles["title"]}>Ends in</label>
+        {timeEnd && (
+          <div className={`${styles[isActive]} ${styles["timer"]} `}>
+            <Countdown onComplete={onTimeEnd} date={timeEnd} />
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return <div className={styles["product-info-timer"]}></div>;
+  }
 }
