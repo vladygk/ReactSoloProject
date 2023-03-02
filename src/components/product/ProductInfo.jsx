74fa647a9d6580868997cbc029f3ecaceeac1e7f
Vladislav Kotsev <123163924+SamuilNikolov00@users.noreply.github.com>
2023-03-02 10:50:25 +0200

    Changed structure

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import styles from "./ProductInfo.module.scss"
-import ProductInfoTitle from "./ProductInfoTitle"
-import ProductInfoPrice from "./ProductInfoPrice"
-import ProductInfoStatus from "./ProductInfoStatus"
-import ProductInfoLikes from "./ProductInfoLikes"
-import ProductInfoCreator from "./ProductInfoCreator"
-import ProductInfoTimer from "./ProductInfoTimer"
-import { Grid, Stack } from '@mui/material'
+import React from "react";
+import styles from "./ProductInfo.module.scss";
+import ProductInfoTitle from "./ProductInfoTitle";
+import ProductInfoPrice from "./ProductInfoPrice";
+import ProductInfoStatus from "./ProductInfoStatus";
+import ProductInfoLikes from "./ProductInfoLikes";
+import ProductInfoCreator from "./ProductInfoCreator";
+import ProductInfoTimer from "./ProductInfoTimer";
+import { Grid, Stack } from "@mui/material";
 export default function ProductInfo({
-    title,
-    creator,
-    price,
-    currency,
-    likes,
-    onTimeEnd,
-    timeEnd,
-    isLive
-    }) {
+  title,
+  creator,
+  price,
+  currency,
+  likes,
+  onTimeEnd,
+  timeEnd,
+  isLive,
+}) {
   return (
-    <div className='product-info'>
-        <ProductInfoTitle text={title}/>
-
-        {isLive && <ProductInfoStatus/>}
-        <Stack className="stats">
-        <ProductInfoPrice amount={price} currency={currency} />
-        <ProductInfoLikes amount={likes}/>
-        </Stack>
-        <Grid columns={{ xs: 7, md: 5 }} container>
-            <Grid item> <ProductInfoCreator name={creator.name} avatar={cretor.avatar} verified={creator.verified}/></Grid>
-            <Grid item> <ProductInfoTimer timeEnd={timeEnd} onTimeEnd={onTimeEnd}/></Grid>
+    
+      <Grid className="product-info" columns={{ xs: 7, md: 5 }} container>
+        <Grid item>
+          <ProductInfoTitle text={title} />
         </Grid>
-       
-       
-    </div>
-  )
+
+        {isLive && (
+          <Grid item>
+            <ProductInfoStatus />
+          </Grid>
+        )}
+        <Grid item>
+          <Stack className="stats">
+            <ProductInfoPrice amount={price} currency={currency} />
+            <ProductInfoLikes amount={likes} />
+          </Stack>
+        </Grid>
+
+        <Grid item>
+          {" "}
+          <ProductInfoCreator
+            name={creator.name}
+            avatar={creator.avatar}
+            verified={creator.verified}
+          />
+        </Grid>
+        <Grid item>
+          {" "}
+          <ProductInfoTimer timeEnd={timeEnd} onTimeEnd={onTimeEnd} />
+        </Grid>
+      </Grid>
+    
+  );
 }
---
+++ src/components/product/ProductTabs.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./ProductTabs.module.scss";
+
+export default function ProductTabs() {
+  return (
+    <div></div>
+  )
+}
