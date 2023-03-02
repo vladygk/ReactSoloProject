964aebeaf1936cdb083ea7c7f7d55d3b2a8e4e60
Vladislav Kotsev <vladygk8@gmaila.com>
2023-03-02 16:29:30 +0200

    fix props

--- src/components/product/ProductActions.jsx
+++ src/components/product/ProductActions.jsx
@@ -2,17 +2,17 @@ import { Button, Grid } from '@mui/material'
 import React from 'react'
 import styles from "./ProductActions.module.scss"
 
-export default function ProductActions(isLive,
+export default function ProductActions({isLive,
 currency,
 buyAmount,
 bidAmount,
 onBuy,
-onBid) {
+onBid}) {
   return (
     <div className={styles["product-actions"]}>
 <Grid container>
-    <Grid  sx={7} item><Button disabled={isLive? true: false} onClick={onBuy} className={styles["button"]}>{`Buy for ${buyAmount} ${currency}`}</Button></Grid>
-    <Grid  sx={5} item><Button disabled={isLive? true: false} onClick={onBid} className={styles["button"]}>{`Place a Bid fro ${bidAmount} ${currency}`}</Button></Grid>
+    <Grid  sx={7} item><Button variant='contained' disabled={isLive? true: false} onClick={onBuy} className={styles["button"]}>{`Buy for ${buyAmount} ${currency}`}</Button></Grid>
+    <Grid  sx={5} item><Button variant='outlined' disabled={isLive? true: false} onClick={onBid} className={styles["button"]}>{`Place a Bid fro ${bidAmount} ${currency}`}</Button></Grid>
 </Grid>
     </div>
   )
